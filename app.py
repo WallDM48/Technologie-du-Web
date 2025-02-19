990243742743c82cc80e6fcd974e6db17086518f
WallDM48 <venimon@abv.bg>
2025-02-19 07:22:54 +0200

    add examples

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS  # Добавяне на CORS
+
+app = Flask(__name__)
+CORS(app)  # Активираме CORS
+
+@app.route('/perform-addition', methods=['POST'])
+def perform_addition():
+    try:
+        # Получаваме JSON данни
+        data = request.get_json()
+        num1 = data['num1']
+        num2 = data['num2']
+        
+        # Изчисляваме сумата
+        result = num1 + num2
+        
+        return jsonify({"result": result})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
